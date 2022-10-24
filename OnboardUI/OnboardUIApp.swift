90c21ba73d6b9e232d09090b0c0fa3ac1e6277dd
ChristopherSamso <chsa6996@colorado.edu>
2022-10-24 11:01:57 -0600

    Initial Commit

---
+++ OnboardUI/OnboardUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardUIApp.swift
+//  OnboardUI
+//
+//  Created by Christopher Samso on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct OnboardUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
